3254a5400b9a16fe6ff8dd8d7b79cbf453b22fa9
nurjauharmuslih <nur.muslih@students.amikom.ac.id>
2020-04-11 00:55:21 +0700

    Latihan Inheritance

---
+++ 03/LatihanInheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Nur", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Jauhar", 30, "191128", "Math");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Muslih", 19, "2832", "muslih@amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
